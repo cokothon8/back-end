10eb70a57431027a2fc6a5913e22f5a7c79755db
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2024-09-28 18:45:39 +0900

    feat: orm세팅, db모델링, 토큰로그인 구현

--- code/database.py
+++ code/database.py
@@ -1,55 +1,33 @@
-# import os
-from dotenv import load_dotenv
-# import aiomysql
-# from typing import Optional
-import logging
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from starlette.config import Config
 
-# 환경 변수 로드 및 로깅 설정
-load_dotenv()
-logging.basicConfig(level=logging.INFO)
+config = Config('.env')
+SQLALCHEMY_DATABASE_URL = config('SQLALCHEMY_DATABASE_URL')
 
-# # 환경 변수에서 MySQL 연결 정보 가져오기
-# DB_HOST = os.getenv('DB_HOST')
-# DB_PORT = os.getenv('DB_PORT')
-# DB_USER = os.getenv('DB_USER')
-# DB_PASSWORD = os.getenv('DB_PASSWORD')
-# DB_NAME = os.getenv('DB_NAME')
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# if None in (DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME):
-#     raise ValueError(".env에 DB 환경변수 셋팅이 제대로 진행되지 않았습니다.")
+Base = declarative_base()
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+Base.metadata = MetaData(naming_convention=naming_convention)
 
-class Database:
-    # def __init__(self):
-    #     self._pool: Optional[aiomysql.Pool] = None
 
-    async def connect(self):
-        logging.info("Connecting to MySQL database...")
-        # self._pool = await aiomysql.create_pool(
-        #     host=DB_HOST,
-        #     port=int(DB_PORT),
-        #     user=DB_USER,
-        #     password=DB_PASSWORD,
-        #     db=DB_NAME,
-        #     autocommit=True,
-        #     charset='utf8mb4'
-        # )
-        logging.info("DB 연결 완료")
-
-    async def disconnect(self):
-        # if self._pool:
-        #     self._pool.close()
-        #     await self._pool.wait_closed()
-        logging.info("DB 연결 해제 완료")
-
-    # async def execute_query(self, query: str, params: Optional[tuple] = None) -> int:
-    #     if not self._pool:
-    #         raise RuntimeError("Connection pool is not initialized.")
-
-    #     async with self._pool.acquire() as conn:
-    #         async with conn.cursor(aiomysql.DictCursor) as cursor:
-    #             await cursor.execute(query, params)
-    #             result = await cursor.fetchall() 
-    #             return result
-
-# Database 인스턴스 생성
-database = Database()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
